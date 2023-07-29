6de23e7b6b4abe8d03e6eff377a4501a2687d550
karto <kartofelek007@gmail.com>
2023-07-29 17:07:59 +0200

    zmiana portu

--- app.js
+++ app.js
@@ -79,4 +79,4 @@ app.post('/', async (req, res) => {
     res.sendStatus(404);
 });
 
-app.listen(8080, () => console.log(`Started server at http://localhost:8080`));
+app.listen(3000, () => console.log(`Started server at http://localhost:3000`));
